780962fbd3ee7c7ea393c39283c106287366e3c9
faisa <faisalprofessional1@gmail.com>
2024-02-19 01:41:24 +0530

    hero section completed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,63 +1,57 @@
 import React from 'react'
 
 import Image from "next/image";
-
+import {FaRegHeart, FaRegUserCircle, FaBars } from "react-icons/fa"
+import { BsGlobe2 } from "react-icons/bs";
 import Logo from "../../../public/assets/Logo.png"
 import Heart from "../../../public/assets/Heart.png"
 import Bars from "../../../public/assets/bars.png"
 import Login from "../../../public/assets/Login.png"
 import Language from "../../../public/assets/Language.png"
+import Link from 'next/link';
 
 
 const Navbar = () => {
     return (
         <header className="bg-gray-100  py-1">
-        <div className="container flex justify-between items-center px-3  ">
+        <nav className="flex justify-between items-center px-3  ">
           <div className="flex items-center lg:ml-[10%] cursor-pointer">
             <Image src={Logo} alt="Logo" width={65} />
           </div>
-          <nav className="flex flex-wrap items-center justify-end gap-6 max-md:flex mr-4">
-            <div className="flex mr-3 cursor-pointer">
-              <Image src={Heart} width={25} className="mr-3 " alt="heart-logo" />
-  
-              <a
+          <div className="flex flex-wrap items-center justify-end gap-6 max-md:flex mr-4">
+              <Link
                 href="#"
-                className="mt-4 text-black lg:mt-0 md:mt-0 max-md:hidden"
+                className="flex items-center gap-2 text-black"
               >
-                Favroites
-              </a>
-            </div>
-            <div className="mr-4 flex cursor-pointer max-md:hidden">
-              <Image
-                src={Language}
-                width={25}
-                className="mr-3 "
-                alt="language-logo"
-              />
-              <a href="#" className="mt-4 text-black lg:mt-0 md:mt-0 ">
-                EN . $
-              </a>
-            </div>
-            <div className="ml-1 flex cursor-pointer">
-              <Image src={Login} width={27} className="mr-3 " alt="Login logo" />
-              <a
+                <i className='text-black' ><FaRegHeart size={20} /></i>
+                <h5 className='lg:mt-0 md:mt-0 max-md:hidden'>Favroites</h5>
+              </Link>
+            
+              <Link
                 href="#"
-                className="mt-4 text-black lg:mt-0 md:mt-0 max-md:hidden"
+                className="flex items-center gap-2 text-black max-md:hidden"
               >
-                Login
-              </a>
-            </div>
-            <div className="flex cursor-pointer mr-4">
-              <Image src={Bars} width={60} className="" alt="bars" />
-              <a
+                <i className='text-black' ><BsGlobe2 size={20} /></i>
+                <h5 className='lg:mt-0 md:mt-0'>EN . $</h5>
+              </Link>
+            
+              <Link
                 href="#"
-                className="mt-4 text-black lg:mt-[1.1rem] max-md:hidden"
+                className="flex items-center gap-2 text-black"
               >
-                Menu
-              </a>
-            </div>
-          </nav>
-        </div>
+                <i className='text-black' ><FaRegUserCircle  size={20} /></i>
+                <h5 className='lg:mt-0 md:mt-0 max-md:hidden'>Login</h5>
+              </Link>
+           
+              <div
+                className="flex items-center gap-2 cursor-pointer text-black"
+              >
+                <i className='text-black' ><FaBars  size={20} /></i>
+                <h5 className='lg:mt-0 md:mt-0 max-md:hidden'>Menu</h5>
+              </div>
+          
+          </div>
+        </nav>
       </header>
     )
 }
