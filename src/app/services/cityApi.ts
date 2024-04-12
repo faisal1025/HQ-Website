2d04357e48669656fbec150be9c32c9460f9907d
faisa <faisalprofessional1@gmail.com>
2024-04-12 17:04:41 +0530

    state fixed

--- src/app/services/cityApi.ts
+++ src/app/services/cityApi.ts
@@ -1,49 +1,38 @@
 import axios from "axios";
 import { amenities, city, hotels } from "../Schema";
-// const qs = require('qs');
-
-// const query = qs.stringify({
-//   filters: {
-//     hotels: {
-//         $not: [],
-//     },
-//   },
-// }, {
-//   encodeValuesOnly: true,
-// });
 
 export const baseUrl = process.env.NODE_ENV === 'production' ? process.env.API_BASE_URL_PROD : process.env.API_BASE_URL_DEV
 
-// export const getAllStates = async () => {
-//     const url = `${baseUrl}/states`
-//     const response = (await axios.get(url)).data;
+export const getAllStates = async () => {
+    const url = `${baseUrl}/states`
+    const response = (await axios.get(url)).data;
 
-//     if(response.error){
-//       throw new Error('something went wrong')
-//     }
+    if(response.error){
+      throw new Error('something went wrong')
+    }
 
-//     const cities:city[] = response.data.map((city:any): city => {
-//         const totalNoHotels = city.attributes.hotels.data.length
-//         const totalPrice = city.attributes.hotels.data.reduce((pre: number, nxt: any) => {
-//           return pre += nxt.attributes.price;
-//         }, 0);
+    const cities:city[] = response.data.map((city:any): city => {
+        const totalNoHotels = city.attributes.hotels.data.length
+        const totalPrice = city.attributes.hotels.data.reduce((pre: number, nxt: any) => {
+          return pre += nxt.attributes.price;
+        }, 0);
         
-//         const avgPrice = totalNoHotels ? Math.floor(totalPrice / totalNoHotels) : 0;
+        const avgPrice = totalNoHotels ? Math.floor(totalPrice / totalNoHotels) : 0;
 
-//         return {...city.attributes, 
-//           id: city.id, 
-//           thumbnail: city.attributes.thumbnail.data.attributes.url,
-//           totalNoHotels,
-//           avgPrice
-//         }
-//     })
+        return {...city.attributes, 
+          id: city.id, 
+          thumbnail: city.attributes.thumbnail.data.attributes.url,
+          totalNoHotels,
+          avgPrice
+        }
+    })
     
-//     return {
-//       props: {
-//         cities
-//       }
-//     }
-// }
+    return {
+      props: {
+        cities
+      }
+    }
+}
 
 export const getStateById = async (slug: string, query: string) => {
     const url = `${baseUrl}/states/${slug}?${query}`
